d22b51d5fc5b6c3bdb392800b17cafb2f23bc4e0
hieuta1 <shoppecuahieuta@gmail.com>
2025-12-14 12:05:53 +0700

    fix tiếp

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,12 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend Project1 OK 🚀");
 });
 
-// ✅ BẮT BUỘC DÙNG process.env.PORT
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
