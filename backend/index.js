9d244ddd6aa993088e87c82c76ae45ccc799da9f
hieuta1 <shoppecuahieuta@gmail.com>
2025-12-14 12:02:46 +0700

    Fix port for Render

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,12 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
 
-// ✅ ROUTE TEST BACKEND
 app.get("/", (req, res) => {
   res.send("Backend Project1 OK 🚀");
 });
 
-// (tuỳ chọn) route test DB
-app.get("/api/time", async (req, res) => {
-  res.json({ time: new Date() });
-});
-
+// ✅ BẮT BUỘC DÙNG process.env.PORT
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
